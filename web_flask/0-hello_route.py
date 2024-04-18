fbce92c748ed4d16236416f892e2db7b2f953a16
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-18 15:02:13 -0700

    update 0-hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/0.0.0.0', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """define a function that prints HEllo HBNB"""
-    name = request.args.get("name",  "HBNB")
-    return f'Hello, {escape(name)}!'
+    return 'Hello HBNB'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
