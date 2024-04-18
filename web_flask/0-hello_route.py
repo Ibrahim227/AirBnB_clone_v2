8ea77dd7ec2b3fc80309b22b364f016ea9c063fb
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-18 14:56:24 -0700

    update 0-hello_route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Import the required module"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('0.0.0.0', strict_slashes=False)
+def hello():
+    name = request.args.get("name",  "HBNB")
+    return f'Hello, {escape(name)}!'
+
+
+if __name__ == "__main__":
+    app.run()
