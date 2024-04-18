1a959380505e25d6b31044994c58a39698016902
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-18 15:14:30 -0700

    update 2-c_route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Import the required module"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """define a function that prints HEllo HBNB"""
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Prints HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Prints HBNB"""
+    return f'C {escape(text)}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
