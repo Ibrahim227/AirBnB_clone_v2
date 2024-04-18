77a92f531567e983b21eb23d275a2d067c6bf9f3
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-18 15:05:41 -0700

    update 1-hbnb_route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Import the required module"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """define a function that prints HEllo HBNB"""
+    return 'Hello HBNB'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
