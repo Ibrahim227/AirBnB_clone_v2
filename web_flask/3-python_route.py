3a85ab765b16974132f209186513a17a185b03bb
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-18 15:38:07 -0700

    update 3-pyton_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Import the required module"""
+from flask import Flask
+from urllib.parse import unquote
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """define a function that prints HEllo HBNB"""
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Prints HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Prints HBNB"""
+    text = unquote(text)
+    return f'C {}'.format(text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>',  strict_slashes=False)
+def pythoniscool(text):
+    """Prints Python is cool"""
+    text = unquote(text)
+    return f'Python {}'.format(text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
