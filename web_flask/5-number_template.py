3ba081b943e938d1872cb622d20760bb39acba34
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-19 07:38:12 -0700

    update 5-number_template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Import the required module"""
+from flask import Flask
+from urllib.parse import unquote
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """define a function that prints HEllo HBNB"""
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Prints HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Prints C is fun"""
+    text = unquote(text)
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>',  strict_slashes=False)
+def python_is_cool(text):
+    """Prints Python is cool"""
+    text = unquote(text)
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numberiscool(n):
+    """Prints number only if it is an integer"""
+    return f'{n} is a number'
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numberiscool(n):
+    """Prints number only if it is an integer"""
+    return f'{n} is a number'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
