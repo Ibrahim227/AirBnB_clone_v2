a6bea00848705a4ebd8388f4ebb0d84e4b1402e5
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-21 10:03:32 -0700

    update 5-number_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,51 +1,46 @@
 #!/usr/bin/python3
-"""Import the required module"""
 from flask import Flask, render_template
-from urllib.parse import unquote
 
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """define a function that prints HEllo HBNB"""
-    return 'Hello HBNB'
+def hello_hbnb():
+    """Function called through the / route."""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Prints HBNB"""
+    """Function called through the /hbnb route."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """Prints C is fun"""
-    text = unquote(text)
-    return 'C {}'.format(text.replace('_', ' '))
+def c_route(text):
+    """Function called through the /c/<text> route."""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
-@app.route('/python/<text>',  strict_slashes=False)
-def python_is_cool(text):
-    """Prints Python is cool"""
-    text = unquote(text)
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """Function called through the /python/<text> route."""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def numberiscool(n):
-    """Prints number only if it is an integer"""
-    return f'{n} is a number'
+def number_route(n):
+    """Function that displays "n is a number" if n is indeed an integer."""
+    return '{:d} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbertemplate(n):
-    """Display an HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template('5-number_template.html', n=n)
+def number_template(n):
+    """Function to display an HTML page only if n is an integer"""
+    return render_template('5-number_template.html', n=n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
